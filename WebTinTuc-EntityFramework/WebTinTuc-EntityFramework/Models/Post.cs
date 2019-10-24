893b745a86ef27966e8e7631865f99ee23224f93
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-10-24 09:35:21 +0700

    Step1

---
+++ WebTinTuc-EntityFramework/WebTinTuc-EntityFramework/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTinTuc_EntityFramework.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public string PostTeaser { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
